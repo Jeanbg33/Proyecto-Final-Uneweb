bb4c8622ddaac63fa521fdb50e7100ece7be728c
jeanbg33 <jeanbogadi33@gmail.com>
2025-09-24 16:17:59 -0400

    feat: error en BD settings

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
