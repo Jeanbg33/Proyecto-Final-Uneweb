219cdd27c8df1325052a2f11b2264bd1e5f71172
jeanbg33 <jeanbogadi33@gmail.com>
2025-09-24 16:56:03 -0400

    Feat database

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -92,7 +92,6 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
